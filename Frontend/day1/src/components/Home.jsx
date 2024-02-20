e83d22fd427479829a6b92186a59484c67c2e3ce
ARUNKUMARO15 <113664088+ARUNKUMARO15@users.noreply.github.com>
2024-02-20 11:52:50 +0530

    Add files via upload

---
+++ Frontend/day1/src/components/Home.jsx
@@ -0,0 +1,20 @@
+// import React from "react";
+import '../assets/css/home.css';
+function Home()
+{
+    return(
+   <>
+    <div id="homeid">
+        <div id="home_admindiv">
+        <img src="https://img.freepik.com/premium-photo/program-designer-developer-ai-generative_199064-2141.jpg?w=740" id="home_adminpic"></img>
+        </div>
+        <div id="home_userdiv">
+        <img src="https://img.freepik.com/free-vector/cruise-concept-illustration_114360-9339.jpg?w=1060&t=st=1708339326~exp=1708339926~hmac=20386fc5fa8c97c2fc4453bc7cb0bc584a9660e276aae29f6342ed970b3e3c69" id="home_userpic"></img>
+    </div>
+    </div>
+
+    </>
+    )
+}
+
+export default Home;
